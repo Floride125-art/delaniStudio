a85e87bfedb62b1604389e018ac8665020c317c1
Floride125-art <FTuyisenge120@daviscollege.com>
2020-10-03 20:46:33 +0200

    added javascript to some icons in service section to enable toggle

---
+++ js/index.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $("#Design").click(function(){
+        $("#Design").toggle();
+        $("#DesignT").toggle();
+    });
+    $("#DesignT").click(function(){
+        $("#Design").toggle();
+        $("#DesignT").toggle();
+    });
+    $("#development").click(function(){
+        $("#development").toggle();
+        $("#developementT").toggle();
+    });
+    $("#developementT").click(function(){
+        $("#development").toggle();
+        $("#developementT").toggle();
+    });
+    $("#planning").click(function(){
+        $("#planning").toggle();
+        $("#planningT").toggle();
+    });
+    $("#planningT").click(function(){
+        $("#planning").toggle();
+        $("#planningT").toggle();
+    });
+
+
+});
