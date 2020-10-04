ff97b0415b164bda32358f72eb136fc919cbe619
Floride125-art <FTuyisenge120@daviscollege.com>
2020-10-04 10:10:02 +0200

    added java script to enable the hover effect on pictures

--- js/index.js
+++ js/index.js
@@ -26,3 +26,65 @@ $(document).ready(function(){
 
 
 });
+
+$(document).ready(function(){
+    $("#pic1").hover(function(){
+        $(".box1").css("opacity", 0.4);
+        $(".box1").fadeIn(800);
+        $(".box1").fadeOut(100).stop();
+
+
+       
+    });
+    $("#pic2").hover(function(){
+        $(".box2").css("opacity", 0.4);
+        $(".box2").fadeIn(800);
+        $(".box2").fadeOut(100).stop();
+
+        
+    });
+    $("#pic3").hover(function(){
+        $(".box3").css("opacity", 0.4);
+        $(".box3").fadeIn(800);
+        $(".box3").fadeOut(100).stop();
+
+        
+    });
+    $("#pic4").hover(function(){
+        $(".box4").css("opacity", 0.4);
+        $(".box4").fadeIn(800);
+        $(".box4").fadeOut(100).stop();
+
+        
+    });
+    $("#pic5").hover(function(){
+        $(".box5").css("opacity", 0.4);
+        $(".box5").fadeIn(800);
+        $(".box5").fadeOut(100).stop();
+
+        
+    });
+    $("#pic6").hover(function(){
+        $(".box6").css("opacity", 0.4);
+        $(".box6").fadeIn(800);
+        $(".box6").fadeOut(100).stop();
+
+        
+    });
+    $("#pic7").hover(function(){
+        $(".box7").css("opacity", 0.4);
+        $(".box7").fadeIn(800);
+        $(".box7").fadeOut(100).stop();
+
+        
+    });
+    $("#pic8").hover(function(){
+        $(".box8").css("opacity", 0.4);
+        $(".box8").fadeIn(800);
+        $(".box8").fadeOut(100).stop();
+
+        
+    });
+
+
+});
